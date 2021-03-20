208975e5f6d5aaa62ab77121df631f23552dfa3f
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-03-20 15:39:47 -0400

    added dom traversal

--- app.js
+++ app.js
@@ -4,3 +4,19 @@
 // var number = 5;
 // var combined = firstName + number;
 // console.log(typeof null);
+
+var js = document.getElementById("js");
+var hello = document.getElementById("hello");
+var div = document.getElementById("div");
+var something = document.getElementById("something");
+var fruits = document.getElementById("fruits");
+
+// console.log(js);
+// console.log(hello);
+// console.log(something);
+if (div.children[1].children[0].textContent === "bananas") {
+  console.log("yes bananas");
+} else {
+  console.log("no bananas");
+}
+// console.log(fruits.parentElement.parentElement);
